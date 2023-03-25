3258715f9cdb77c7ce4c267798d129eeee23aaf6
GlobuloBianco <wufrancescojh@gmail.com>
2023-03-25 21:48:03 +0100

    v0.06 signup improvement 💃

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -12,12 +12,21 @@ export class SignupComponent implements OnInit {
     email: string = "";
     username: string = "";
     password: string = "";
+    showPassword: boolean = false;
+    usernameValid: boolean = false;
+    emailValid: boolean = false;
+    passwordValid: boolean = false;
     constructor(private http: HttpClient, private router: Router) { }
 
     ngOnInit(): void {
     }
 
     onSubmit() {
+        if (!(this.usernameValid && this.emailValid && this.passwordValid)) {
+            alert("Please enter valid input.");
+            return;
+          }
+
         const body = { email: this.email, username: this.username, password: this.password };
         this.http.post('http://localhost:8080/api/auth/signup', body).subscribe(
             response => {
@@ -30,6 +39,15 @@ export class SignupComponent implements OnInit {
         );
     }
 
-    //-----Validazione Form-----//
+    togglePassword() {
+        let passwordCheck = document.getElementById('passwordCheck') as HTMLInputElement;
+        // show - hide password
+        !this.showPassword ? (passwordCheck.setAttribute('type', 'text'),this.showPassword = true) : (passwordCheck.setAttribute('type', 'password'),this.showPassword = false);
+    }
 
+    //-----Validazione Regex-----//
+    checkInput(input: string): boolean {
+        const regex = /^[a-zA-Z0-9]{3,16}$/; //solo numeri e lettere | min-max 3-16 caratteri
+        return regex.test(input);
+    }
 }
